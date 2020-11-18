6c72fac51a33b915a3988033d85a1942c03be676
celrati <m.charif.achraf@gmail.com>
2020-11-18 17:12:09 +0100

    finish crud local diaries using recoiljs

--- notediary/src/components/diaries/DiaryCreate.js
+++ notediary/src/components/diaries/DiaryCreate.js
@@ -1,10 +1,58 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import { noteState, diaryState } from '../atoms';
+import { useRecoilState } from "recoil"
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { useHistory } from "react-router-dom";
 
 const DiaryCreate = () => {
+
+    const [diaryList, setDiaryList] = useRecoilState(diaryState);
+    const [form] = Form.useForm();
+    let history = useHistory();
+    const newNote = useCallback(
+        (values) => {
+            let max = 0;
+            diaryList.forEach(note => {
+                if (note.id > max) {
+                    max = note.id;
+                }
+            });
+            max += 1;
+            setDiaryList([...diaryList, { id: max, name: values.diaryName, content: values.diaryContent }])
+            history.push('/diarylist');
+        }
+        , []);
+
     return (
-        <div>
-            Diary Create hello
-        </div>
+        <Form form={form} name="horizontal_login" layout="inline" style={{ marginTop: '3%' }} onFinish={newNote}>
+            <Form.Item
+                name="diaryName"
+                rules={[{ required: true, message: 'Please input the name of your diary !' }]}
+            >
+                <Input placeholder="diaryName" />
+            </Form.Item>
+            <Form.Item
+                name="diaryContent"
+                rules={[{ required: true, message: 'Please input some content !' }]}
+            >
+                <Input
+
+                    placeholder="diaryContent"
+                />
+            </Form.Item>
+            <Form.Item shouldUpdate={true}>
+                {() => (
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+
+                    >
+                        New Diary
+            </Button>
+                )}
+            </Form.Item>
+        </Form>
     );
 };
 
--- notediary/src/components/diaries/DiaryList.js
+++ notediary/src/components/diaries/DiaryList.js
@@ -1,9 +1,61 @@
 import React, { useCallback, useState } from 'react';
+import { noteState, diaryState } from '../atoms';
+import { useRecoilState } from "recoil"
+import { Row, Col, Card, Skeleton, notification } from 'antd';
+import { DownOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
+const { Meta } = Card;
 const DiaryList = () => {
+
+    const [diaryList, setDiaryList] = useRecoilState(diaryState);
+    
+
+
+    const deleteDiary = useCallback(
+        id => {
+            const newList = diaryList.filter( diary => diary.id !== id);
+            setDiaryList(newList);
+        }
+        ,[diaryList]);
+
+    const showDiary = useCallback(
+        (name, content) => {
+            notification.info({
+                message: `diary's name : ${name}`,
+                description: `content : ${content}`,
+                placement : 'topRight',
+              });
+        }
+    );
+
+
     return (
-        <div>
-            Diary List hello
+        <div className="site-card-wrapper" style={{ marginTop: '2%' }}>
+            <Row gutter={16}>
+
+                {
+                    diaryList.map(diary => (
+                        <Col span={8} style={{ marginTop: '2%' }}>
+                            <Card
+                            hoverable
+                                style={{ width: 300, marginTop: 16 }}
+                                actions={[
+                                    <DownOutlined key="show" onClick={() => showDiary(diary.name, diary.content)}/>,
+                                    <DeleteOutlined key="delete" onClick={() => deleteDiary(diary.id)}/>,
+                                ]}
+                            >
+                                <Skeleton loading={false} avatar active>
+                                    <Meta
+                                        title={diary.name}
+                                        description={diary.content}
+                                    />
+                                </Skeleton>
+                            </Card>
+                        </Col>
+                    ))
+                }
+
+            </Row>
         </div>
     );
 };
